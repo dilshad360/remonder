c1d3702e8a9402f41dd150aa1f1643773ece18c5
dilshad360 <dilshad360.dev@gmail.com>
2023-04-26 22:06:17 +0530

    feat: datetime

--- pages/index.js
+++ pages/index.js
@@ -16,9 +16,11 @@ import {
   doc,
 } from "firebase/firestore";
 import { db } from "@/firebase/firebase";
+import moment from "moment";
 
 export default function Home() {
   const [reminderInput, setReminderInput] = useState("");
+  const [datetimeInput, setDatetimeInput] = useState("");
   const [reminderList, setReminderList] = useState([]);
 
   const { authUser, isLoading, signOut } = useAuth();
@@ -34,14 +36,16 @@ export default function Home() {
   }, [authUser, isLoading]);
 
   const addReminder = async () => {
+    console.log(datetimeInput)
     try {
       const docRef = await addDoc(collection(db, "reminders"), {
         owner: authUser.uid,
         content: reminderInput,
+        datetime: datetimeInput,
         completed: false,
       });
-      fetchReminder(authUser.uid)
-      setReminderInput("")
+      fetchReminder(authUser.uid);
+      setReminderInput("");
     } catch (error) {
       console.error(error);
     }
@@ -49,41 +53,41 @@ export default function Home() {
 
   const deleteReminder = async (docId) => {
     try {
-      await deleteDoc(doc(db, "reminders", docId))
-      fetchReminder(authUser.uid)
+      await deleteDoc(doc(db, "reminders", docId));
+      fetchReminder(authUser.uid);
     } catch (error) {
       console.error(error);
     }
-  }
+  };
 
-  const markAsCompletedHandler = async (event , docId) =>{
+  const markAsCompletedHandler = async (event, docId) => {
     try {
       const docRef = doc(db, "reminders", docId);
       await updateDoc(docRef, {
-        completed: event.target.checked
-      })
-      fetchReminder(authUser.uid)
+        completed: event.target.checked,
+      });
+      fetchReminder(authUser.uid);
     } catch (error) {
       console.error(error);
     }
-  }
+  };
 
   const onKeyUp = (event) => {
     if (event.key === "Enter" && reminderInput.length > 0) {
       addReminder();
     }
-  }
+  };
 
   const fetchReminder = async (uid) => {
     try {
       const q = query(collection(db, "reminders"), where("owner", "==", uid));
       const querySnapshot = await getDocs(q);
-      let data = []
+      let data = [];
       querySnapshot.forEach((doc) => {
         console.log(doc.id, " => ", doc.data());
-        data.push({...doc.data(), id:doc.id})
+        data.push({ ...doc.data(), id: doc.id });
       });
-      setReminderList(data)
+      setReminderList(data);
     } catch (error) {
       console.error(error);
     }
@@ -106,48 +110,74 @@ export default function Home() {
             <h1 className="text-4xl md:text-5xl font-bold">Remonder's</h1>
           </div>
           <div className="flex justify-center items-center mt-6 gap-2">
-            <input
-              type="text"
-              placeholder={`👋🏻 Hello ${authUser.username} , Write your reminder here`}
-              value={reminderInput}
+            <div className="flex flex-col w-full grow gap-1">
+              <input
+                type="text"
+                placeholder={`👋🏻 Hello ${authUser.username} , What should i remind you`}
+                value={reminderInput}
+                onChange={(e) => {
+                  setReminderInput(e.target.value);
+                }}
+                onKeyUp={onKeyUp}
+                className="font-semibold border-[2px] border-black h-[40px] px-4 shadow-md rounded-md focus-visible:outline-yellow-400 text-lg transition-all duration-200"
+              />
+              <input
+              value={datetimeInput}
               onChange={(e) => {
-                setReminderInput(e.target.value);
+                setDatetimeInput(e.target.value);
               }}
-              onKeyUp={onKeyUp}
-              className="font-semibold border-[2px] border-black h-[60px] grow px-4 shadow-md rounded-md focus-visible:outline-yellow-400 text-lg transition-all duration-200"
-            />
+                type="datetime-local"
+                className="font-semibold border-[2px] border-black h-[40px]  px-4 shadow-md rounded-md focus-visible:outline-yellow-400 text-lg transition-all duration-200"
+              />
+            </div>
             <button
               onClick={addReminder}
-              className="bg-black h-[60px] w-[60px] text-white  flex justify-center items-center shadow-md rounded-md hover:bg-black/90 active:bg-yellow-400 active:scale-90 transition-transform"
+              className="bg-black h-[80px] w-[80px] text-white  flex justify-center items-center shadow-md rounded-md hover:bg-black/90 active:bg-yellow-400 active:scale-90 transition-transform"
             >
-              <FaPlus />
+              <FaPlus size={20} />
             </button>
           </div>
         </div>
         <div className="my-6">
-          {reminderList.length > 0 && reminderList.map((reminder, index) => (
-            <div key={reminder.id} className="flex items-center justify-between mt-4">
-              <div className="flex items-center gap-3 ">
-                <input
-                  id={`reminder-${reminder.id}`}
-                  type="checkbox"
-                  className="h-4 w-4 accent-green-400 rounded-lg "
-                  checked={reminder.completed}
-                  onChange={(e)=>markAsCompletedHandler(e,reminder.id)}
-                />
-                <label htmlFor={`reminder-${reminder.id}`} className={`font-medium ${reminder.completed ? "line-through" : ""}`}>
-                  {reminder.content}
-                </label>
+          {reminderList.length > 0 &&
+            reminderList.map((reminder, index) => (
+              <div
+                key={reminder.id}
+                className="flex items-center justify-between mt-4"
+              >
+                <div className="flex items-center gap-3 ">
+                  <input
+                    id={`reminder-${reminder.id}`}
+                    type="checkbox"
+                    className="h-5 w-5 accent-green-400 rounded-lg "
+                    checked={reminder.completed}
+                    onChange={(e) => markAsCompletedHandler(e, reminder.id)}
+                  />
+                  <div className="flex flex-col">
+                  <label
+                    htmlFor={`reminder-${reminder.id}`}
+                    className={`font-medium text-lg ${
+                      reminder.completed ? "line-through" : ""
+                    }`}
+                  >
+                    {reminder.content}
+                  </label>
+                  { reminder.datetime &&
+                    <span className={`text-xs font-sans font-medium text-gray-500 ${
+                      reminder.completed ? "line-through" : ""
+                    } `}>{moment(reminder.datetime).format("MMM D, YYYY, h:mm a")} </span>
+                  }
+                  </div>
+                </div>
+                <div className="flex items-center gap-3 ">
+                  <MdDeleteForever
+                    onClick={() => deleteReminder(reminder.id)}
+                    size={24}
+                    className="text-red-400 cursor-pointer hover:text-red-600"
+                  />
+                </div>
               </div>
-              <div className="flex items-center gap-3 ">
-                <MdDeleteForever
-                  onClick={()=>deleteReminder(reminder.id)}
-                  size={24}
-                  className="text-red-400 cursor-pointer hover:text-red-600"
-                />
-              </div>
-            </div>
-          ))}
+            ))}
         </div>
       </div>
     </main>
